3712227c215fb3845ffd50f60b62c4c33cc69f54
Anthony LoGreco <aclogreco@gmail.com>
2014-11-07 00:11:54 -0500

    Add exercise 5.

---
+++ ex5.py
@@ -0,0 +1,24 @@
+# ex5.py
+"""
+Exercise 5 -- Learn Python the Hard Way -- Zed A. Shaw 
+A.C. LoGreco
+"""
+
+my_name = 'Anthony C. LoGreco'
+my_age = 32
+my_height = 66  # inches
+my_weight = 185  # lbs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "Actually that's not too heavy. ;-)"
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee. ;-)" % my_teeth
+
+print "If I add %d, %d, and %d I get %d." % (
+    my_age, my_height, my_weight, my_age + my_height + my_weight)
+
