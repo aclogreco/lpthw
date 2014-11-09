9a8e33d1755a48baa04aae3bca22374d93160a05
Anthony LoGreco <aclogreco@gmail.com>
2014-11-09 05:02:55 -0500

    Add exercise 7.

---
+++ ex7.py
@@ -0,0 +1,27 @@
+# ex7.py
+"""
+Exercise 7 -- Learn Python the Hard Way -- Zed A. Shaw
+A.C. LoGreco
+"""
+
+print "Mary had a little lamb."
+print "Its fleece was white as %s." % 'snow'
+print "And everywhere that Mary went."
+print "." * 10
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
+
