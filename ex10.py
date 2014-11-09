acf2d986bf350fad1f3207d006095544df8c22ff
Anthony LoGreco <aclogreco@gmail.com>
2014-11-09 05:40:59 -0500

    Add exercise 10.

---
+++ ex10.py
@@ -0,0 +1,22 @@
+# ex10.py
+"""
+Exercise 10 -- Learn Python the Hard Way -- Zed A. Shaw
+A.C. LoGreco
+"""
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+
