8883c55bcac0e7c9432247b95afa2aaa085fc1b5
Anthony LoGreco <aclogreco@gmail.com>
2014-11-10 03:07:12 -0500

    Add and finish exercise 11.

---
+++ ex11.py
@@ -0,0 +1,15 @@
+# ex11.py
+"""
+Exercise 11 -- Learn Python the Hard Way -- Zed A. Shaw
+A.C. LoGreco
+"""
+
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall, and %r heavy." % (age, height, weight)
+
