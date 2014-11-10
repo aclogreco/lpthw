6252bc63e34c3181215fa4141a7034755eeb264d
Anthony LoGreco <aclogreco@gmail.com>
2014-11-10 03:47:46 -0500

    Add exercise 13.

---
+++ ex13.py
@@ -0,0 +1,15 @@
+# ex13.py
+"""
+Exercise 13 -- Learn Python the Hard Way -- Zed A. Shaw
+A.C. LoGreco
+"""
+
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+
