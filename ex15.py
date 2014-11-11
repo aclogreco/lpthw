53220418df62af9ba892288523cee199d63faf54
Anthony LoGreco <aclogreco@gmail.com>
2014-11-10 22:48:02 -0500

    Finish exercise 15 study drills.

--- ex15.py
+++ ex15.py
@@ -6,17 +6,27 @@ A.C. LoGreco
 
 from sys import argv
 
+# unpack command line arguments into variables
 script, filename = argv
 
+# open the file specified by the filename
 txt = open(filename)
 
+# print the file to std out
 print "Here's your file %r:" % filename
 print txt.read()
 
+# ask the user for a file name
 print "Type the filename again:"
 file_again = raw_input("> ")
 
+# open the file specified by the user
 txt_again = open(file_again)
 
+# print that file to std out
 print txt_again.read()
+
+# close files
+txt.close()
+txt_again.close()
 
