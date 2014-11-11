c5bdda721072a3f16a06eae9fc19b72b2baa5483
Anthony LoGreco <aclogreco@gmail.com>
2014-11-10 22:39:43 -0500

    Add exercise 15.

---
+++ ex15.py
@@ -0,0 +1,22 @@
+# ex15.py
+"""
+Exercise 15 -- Learn Python the Hard Way -- Zed A. Shaw
+A.C. LoGreco
+"""
+
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print "Here's your file %r:" % filename
+print txt.read()
+
+print "Type the filename again:"
+file_again = raw_input("> ")
+
+txt_again = open(file_again)
+
+print txt_again.read()
+
