cd735d9f7b396a9014f76cbccc64b50d41e89952
Anthony LoGreco <aclogreco@gmail.com>
2014-11-11 07:20:40 -0500

    Add exercise 17.

---
+++ ex17.py
@@ -0,0 +1,31 @@
+# ex17.py
+"""
+Exercise 17 -- Learn Python the Hard Way -- Zed A. Shaw
+A.C. LoGreco
+"""
+
+from sys import argv
+from os.path import exists
+
+# unpack cmd line arguments
+script, from_file, to_file = argv
+
+print "Copying from %s to %s" % (from_file, to_file)
+
+in_file = open(from_file)
+indata = in_file.read()
+
+print "The input file is %d bytes long" % len(indata)
+
+print "Does the output file exist? %r" % exists(to_file)
+print "Ready, hit ENTER (RETURN) to continue, CTRL-C to abort."
+raw_input()
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+print "Alright, all done."
+
+out_file.close()
+in_file.close()
+
