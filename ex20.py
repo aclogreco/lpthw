9644c7306bae98e14230971e6649324e54c59277
Anthony LoGreco <aclogreco@gmail.com>
2014-11-13 06:50:26 -0500

    Finish exercise 20, study drill 1.

--- ex20.py
+++ ex20.py
@@ -6,20 +6,25 @@ A.C. LoGreco
 
 from sys import argv
 
+# unpack cmd line arguments
 script, input_file = argv
 
+# print out the contents of file f
 def print_all(f):
     print f.read()
 
 
+# set file cursor position to the begining of file f
 def rewind(f):
     f.seek(0)
 
 
+# print a line from file f
 def print_a_line(line_count, f):
     print line_count, f.readline()
 
 
+# open the file for reading
 current_file = open(input_file)
 
 print "First, let's print the whole file:\n"
