af4ddaaff0956d400bd2dae9fc57806ba780da6d
Anthony LoGreco <aclogreco@gmail.com>
2014-11-14 04:10:22 -0500

    Add exercise 21.

---
+++ ex21.py
@@ -0,0 +1,43 @@
+# ex21.py
+"""
+Exercise 21 -- Learn Python the Hard Way -- Zed A. Shaw
+A.C. LoGreco
+"""
+
+def add(a, b):
+    print "ADDING %d + %d" % (a, b)
+    return a + b
+
+
+def subtract(a, b):
+    print "SUBTRACTING %d - %d" % (a, b)
+    return a - b
+
+
+def multiply(a, b):
+    print "MULTIPLYING %d * %d" % (a, b)
+    return a * b
+
+
+def divide(a, b):
+    print "DIVIDING %d / %d" % (a, b)
+    return a / b
+
+
+print "Let's do some math with just functions!"
+
+age = add(30, 2)
+height = subtract(72, 6)
+weight = multiply(95, 2)
+iq = divide(100, 2)
+
+print "Age: %d, Height: %d, Weight: %d, IQ: %d" % (age, height, weight, iq)
+
+
+# A puzzle for the extra credit
+print "Here is a puzzle."
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+print "That becomes: ", what, "Can you do it by hand?"
+
