cb85e0967227c3f54f24b66308fe3f760ae31142
Anthony LoGreco <aclogreco@gmail.com>
2014-11-19 03:48:30 -0500

    Exercise 33 - finish study drill 1 - convert while loop into function.

--- ex33.py
+++ ex33.py
@@ -4,6 +4,21 @@ Exercise 33 -- Learn Python the Hard Way -- Zed A. Shaw
 A.C. LoGreco
 """
 
+def makeNumList(count):
+    """Creates a list from 0 to (count - 1)"""
+    i = 0
+    numList = []
+    
+    while i < count:
+        print "At the top i is %d" % i
+        numList.append(i)
+        i += 1
+        print "Numbers now: ", numList
+        print "At the bottom i is %d" % i
+    
+    return numList
+
+
 i = 0
 numbers = []
 
@@ -18,5 +33,4 @@ while i < 6:
 print "The numbers: "
 for num in numbers:
     print num
-
 
