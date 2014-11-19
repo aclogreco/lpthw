5d2b446fd019f207c264c12683a46a274828c6a3
Anthony LoGreco <aclogreco@gmail.com>
2014-11-19 03:57:06 -0500

    Exercise 33 - finish study drills 3 and 4.

--- ex33.py
+++ ex33.py
@@ -4,32 +4,32 @@ Exercise 33 -- Learn Python the Hard Way -- Zed A. Shaw
 A.C. LoGreco
 """
 
-def makeNumList(count):
-    """Creates a list from 0 to (count - 1)"""
+def makeNumList(count, inc):
+    """Creates a list from 0 to (count - 1) incremented by inc"""
     i = 0
     numList = []
     
     while i < count:
         print "At the top i is %d" % i
         numList.append(i)
-        i += 1
+        i += inc
         print "Numbers now: ", numList
         print "At the bottom i is %d" % i
     
     return numList
 
 
-numbers = makeNumList(10)
+numbers = makeNumList(10, 2)
 print "The numbers: "
 for num in numbers:
     print num
 
-numbers = makeNumList(8)
+numbers = makeNumList(8, 3)
 print "The numbers: "
 for num in numbers:
     print num
 
-numbers = makeNumList(6)
+numbers = makeNumList(6, 1)
 print "The numbers: "
 for num in numbers:
     print num
